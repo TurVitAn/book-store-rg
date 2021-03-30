ee996d8cda22e58eebf76012d904f10334568244
Vitalii <turzhansky81@gmail.com>
2021-03-31 00:42:58 +0300

    task/BS-01-20: Add Webpacker compiling styles

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,16 @@
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 
-require("jquery")
-require("bootstrap-sass")
-require ("@fortawesome/fontawesome-free/js/all.min")
-require("typeface-open-sans")
+import $ from 'jquery';
+
+window.$ = $;
+window.jQuery = $;
+
+import 'bootstrap-sass/assets/javascripts/bootstrap';
+import "@fortawesome/fontawesome-free/css/all"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
