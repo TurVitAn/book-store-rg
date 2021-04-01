885b0c531ce5e99a3df5321bb3a1ad9cc8fdb811
Vitalii <turzhansky81@gmail.com>
2021-04-01 13:54:42 +0300

    task/BS-02-2: Add validation model Author

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,4 +1,11 @@
 RSpec.describe Author, type: :model do
-  xit { is_expected.to have_many(:author_books).dependent(:destroy) }
-  xit { is_expected.to have_many(:books).through(:author_books).dependent(:destroy) }
+  context 'with associations' do
+    xit { is_expected.to have_many(:author_books).dependent(:destroy) }
+    xit { is_expected.to have_many(:books).through(:author_books).dependent(:destroy) }
+  end
+
+  context 'with validations' do
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+  end
 end
