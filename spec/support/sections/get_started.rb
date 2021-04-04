80c424592072a33cf990c8022ff092b33672ecc3
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2021-04-04 19:42:11 +0300

    task/BS-02-10: Add books#show view with book data

--- spec/support/sections/get_started.rb
+++ spec/support/sections/get_started.rb
@@ -1,5 +1,6 @@
 module Sections
   class GetStarted < SitePrism::Section
+    element :get_started_link, 'a.btn-default'
     element :get_started_button, 'button.btn-default'
     element :greeting, 'h2.h1'
     element :introduction, 'div.mb-res-50>p'
