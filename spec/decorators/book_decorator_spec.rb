4e43fda7a4fba6a21379ec1031c649a503c4d50a
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2021-04-07 11:30:39 +0300

    task/BS-02-20: Write all tests for decorators

--- spec/decorators/book_decorator_spec.rb
+++ spec/decorators/book_decorator_spec.rb
@@ -1,8 +1,24 @@
 RSpec.describe BookDecorator do
-  let(:author) { build(:author, first_name: 'Vitalii', last_name: 'Turzhanskyi') }
-  let(:book) { build(:book, authors: [author]).decorate }
+  let(:author) { create(:author, first_name: 'Vitalii', last_name: 'Turzhanskyi') }
+  let(:book) do
+    create(:book, authors: [author], materials: 'marbled paper', height: 20, width: 13, depth: 5).decorate
+  end
 
   it '#authors_list' do
     expect(book.authors_list).to eq('Vitalii Turzhanskyi')
+  end
+
+  it '#materials_list' do
+    expect(book.materials_list).to eq('Marbled, paper')
+  end
+
+  it '#dimensions' do
+    expect(book.dimensions).to eq('H:20.0" x W:13.0" x D:5.0')
+  end
+
+  it '#short_description' do
+    expect(book.short_description).to eq(
+      book.description.truncate(described_class::SHORT_DESCRIPTION_LENGTH)
+    )
   end
 end
