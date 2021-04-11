14b3fff592a90cd78260bf365a7bcc439584d220
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2021-04-11 13:26:16 +0300

    task/BS-03-9: Create a  Privacy Policy and SEO tags

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,25 @@
+module Users
+  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    def facebook
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+
+      @user.persisted? ? authenticate_user : registrate_user
+    end
+
+    def failure
+      redirect_to root_path
+    end
+
+    private
+
+    def authenticate_user
+      set_flash_message(:notice, :success, kind: I18n.t('devise.provider')) if is_navigational_format?
+      sign_in_and_redirect(@user, event: :authentication)
+    end
+
+    def registrate_user
+      session['devise.facebook_data'] = request.env['omniauth.auth'].except(:extra)
+      redirect_to new_user_registration_path
+    end
+  end
+end
