0b47d7d8a251c0f1b35ff1e960d155a2556cb77c
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2021-04-14 15:31:09 +0300

    task/BS-01-26: Add I18n lazy lookup

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,10 @@ module BookStoreRg
   class Application < Rails::Application
     config.load_defaults 6.1
 
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
+    config.i18n.available_locales = %i[en uk]
+    config.i18n.default_locale = :en
+
     config.generators do |g|
       g.test_framework :rspec
     end
