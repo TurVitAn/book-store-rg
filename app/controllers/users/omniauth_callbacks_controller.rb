dcfd5709391e51a141e44faebfd8350e3e01c57d
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2021-04-15 23:21:56 +0300

    task/BS-03-24: Fix authentication passthru via Omniauth-Facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,19 +7,19 @@ module Users
     end
 
     def failure
-      redirect_to root_path
+      redirect_to root_path, alert: 'Failure. Please try again'
     end
 
     private
 
     def authenticate_user
-      set_flash_message(:notice, :success, kind: I18n.t(:provider)) if is_navigational_format?
+      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
       sign_in_and_redirect(@user, event: :authentication)
     end
 
     def registrate_user
       session['devise.facebook_data'] = request.env['omniauth.auth'].except(:extra)
-      redirect_to new_user_registration_path
+      redirect_to new_user_registration_path, alert: @user.errors.full_messages.join("\n")
     end
   end
 end
