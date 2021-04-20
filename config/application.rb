812a7f19967f69d1133681c89131d7bdb131ac9e
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2021-04-20 19:36:21 +0300

    task/BS-01-27: Fix mentor review

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,6 @@
 require_relative 'boot'
 
-require 'rails'
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'action_mailbox/engine'
-require 'action_text/engine'
-require 'action_view/railtie'
-require 'sprockets/railtie'
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
@@ -18,7 +8,6 @@ module BookStoreRg
   class Application < Rails::Application
     config.load_defaults 6.1
 
-    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
     config.i18n.available_locales = %i[en uk]
     config.i18n.default_locale = :en
 
