9e0cd602fe3db8bb3c4c45bce720d4174e191ac2
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2021-04-22 18:22:48 +0300

    task/BS-04-15: Change specs AddressService

--- spec/services/settings/address_service_spec.rb
+++ spec/services/settings/address_service_spec.rb
@@ -1,32 +1,18 @@
 RSpec.describe Settings::AddressService do
   let(:user) { create(:user) }
-  let(:invalid_billing_params) { attributes_for(:address, zip_code: '', first_name: '!!!') }
-  let(:valid_billing_params) { attributes_for(:address) }
-  let(:invalid_shipping_params) { attributes_for(:address, address_type: 'shipping', city: '') }
-  let(:valid_shipping_params) { attributes_for(:address, address_type: 'shipping') }
+  let(:valid_params) { attributes_for(:address) }
+  let(:invalid_params) { attributes_for(:address, zip_code: '') }
 
   describe '#call' do
-    context 'with valid billing params' do
+    context 'with valid params' do
       it 'returns true' do
-        expect(described_class.new(user: user, params: valid_billing_params).call).to eq(true)
+        expect(described_class.new(user: user, params: valid_params).call).to be true
       end
     end
 
-    context 'with invalid billing params' do
-      it 'returns Array with errors' do
-        expect(described_class.new(user: user, params: invalid_billing_params).call.class).to eq(Array)
-      end
-    end
-
-    context 'with valid shipping params' do
-      it 'returns true' do
-        expect(described_class.new(user: user, params: valid_shipping_params).call).to eq(true)
-      end
-    end
-
-    context 'with invalid shipping params' do
-      it 'returns Array with errors' do
-        expect(described_class.new(user: user, params: invalid_shipping_params).call.class).to eq(Array)
+    context 'with invalid params' do
+      it 'returns false' do
+        expect(described_class.new(user: user, params: invalid_params).call).to be false
       end
     end
   end
