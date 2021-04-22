ceae92ed1f59c974b913d353f5cbb756798de876
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2021-04-22 14:03:55 +0300

    task/BS-04-10: Add refactor AddressService

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,12 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   protect_from_forgery
 
-  before_action :categories, :countries
+  before_action :categories
 
   private
 
   def categories
     @categories ||= Category.all
-  end
-
-  def countries
-    @countries ||= ISO3166::Country.all.sort_by(&:name)
   end
 
   def record_not_found
--- app/services/settings/address_service.rb
+++ app/services/settings/address_service.rb
@@ -1,47 +1,31 @@
 module Settings
   class AddressService
-    attr_reader :success
+    attr_reader :errors
 
-    def initialize(user:, params:, id: nil)
+    def initialize(user:, params:)
       @user = user
-      @params = params
-      @id = id
-      @form = AddressForm.new(@params.except(:addressable))
+      @form = AddressForm.new(params)
+      @params = params.merge(addressable: user)
+      @errors = {}
     end
 
     def call
-      address = Address.find_by(id: id) || Address.new(addressable: user)
+      return save_address if form.valid?
 
-      form_is_valid? ? address.update(params) : add_errors_to_addres(handle_address_status)
+      @errors = form.errors
+      false
     end
 
     private
 
-    attr_reader :id, :user, :params, :form
+    attr_reader :user, :params, :form
 
-    def handle_address_status
-      case params[:address_type]
-      when SettingsPresenter::BILLING_TYPE then check_billing_address_presence
-      when SettingsPresenter::SHIPPING_TYPE then check_shipping_address_presence
-      end
+    def address
+      @address ||= Address.find_by(addressable_id: user.id, address_type: params[:address_type])
     end
 
-    def check_billing_address_presence
-      user.billing_address.presence || user.build_billing_address(params)
-    end
-
-    def check_shipping_address_presence
-      user.shipping_address.presence || user.build_shipping_address(params)
-    end
-
-    def add_errors_to_addres(address)
-      form.errors.each do |attribute, message|
-        address.errors.add(attribute, message)
-      end
-    end
-
-    def form_is_valid?
-      @success = form.valid?
+    def save_address
+      address.nil? ? Address.create(params) : address.update(params)
     end
   end
 end
