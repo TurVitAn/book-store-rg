ccb58ba40726a9b017e3a49f99e886e56f6a401f
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2021-04-23 21:32:27 +0300

    task/BS-02-33: Change AuthorDecorator specs

--- spec/decorators/author_decorator_spec.rb
+++ spec/decorators/author_decorator_spec.rb
@@ -1,7 +1,13 @@
 RSpec.describe AuthorDecorator do
-  let(:author) { create(:author, first_name: 'Vitalii', last_name: 'Turzhanskyi').decorate }
+  let(:first_name) { FFaker::Name.first_name }
+  let(:last_name) { FFaker::Name.last_name }
+  let(:author) { build(:author, first_name: first_name, last_name: last_name).decorate }
 
-  it '#full_name' do
-    expect(author.full_name).to eq('Vitalii Turzhanskyi')
+  describe '#full_name' do
+    context 'when call method' do
+      let(:full_name_result) { "#{first_name} #{last_name}" }
+
+      it { expect(author.full_name).to eq(full_name_result) }
+    end
   end
 end
