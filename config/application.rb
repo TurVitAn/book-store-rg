d3886d81135d1f0fa30d2f2c457a3b70824f67e5
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2021-04-26 21:45:29 +0300

    task/BS-02-42: Remove uk locale

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,6 @@ module BookStoreRg
   class Application < Rails::Application
     config.load_defaults 6.1
 
-    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml')]
-    config.i18n.available_locales = %i[en uk]
-    config.i18n.default_locale = :en
-
     config.generators do |g|
       g.test_framework :rspec
     end
