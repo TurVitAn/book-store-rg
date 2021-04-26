2fd15d6b6bbf68e5332b973d12ee3b35ed1ac9bf
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2021-04-27 00:40:23 +0300

    task/BS-02-44: Use FactoryBot for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,17 @@
 require 'ffaker'
-require './db/seeds/categories'
-require './db/seeds/authors'
-require './db/seeds/books'
+
+CATEGORIES = {
+  mobile_development: 'Mobile Development',
+  photo: 'Photo',
+  web_design: 'Web Design',
+  web_development: 'Web Development'
+}.freeze
+
+CATEGORIES.each_value { |category| Category.create(name: category) } if Category.count.zero?
+
+FactoryBot.create_list(:author, 7) if Author.count.zero?
+
+if Book.count.zero?
+  FactoryBot.create_list(:book, 50, category_id: rand(4),
+                                    authors: [Author.all.sample, Author.all.sample])
+end
--- db/seeds/authors.rb
+++
@@ -1,8 +0,0 @@
-if Author.count.zero?
-  7.times do
-    Author.create!(
-      first_name: FFaker::Name.first_name,
-      last_name: FFaker::Name.last_name
-    )
-  end
-end
