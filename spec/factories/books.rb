c5b8428662af93f498950fb0c287a63d48c88aa4
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2021-04-28 09:13:10 +0300

    task/BS-02-47: Add ffaker random

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :book do
     title { FFaker::Book.title }
     description { FFaker::Book.description(8) }
-    price { rand(1.0..1000.0).floor(2) }
-    height { rand(7.0..40.0).floor(2) }
-    width { rand(1.0..20.0).floor(2) }
-    depth { rand(1.0..7.0).floor(2) }
+    price { FFaker::Random.rand(1.0..1000.0).floor(2) }
+    height {  FFaker::Random.rand(7.0..40.0).floor(2) }
+    width {  FFaker::Random.rand(1.0..20.0).floor(2) }
+    depth {  FFaker::Random.rand(1.0..7.0).floor(2) }
     materials { FFaker::Lorem.word }
     published_at { FFaker::Time.date }
 
