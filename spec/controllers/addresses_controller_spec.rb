2c1d912c8c72c5011c0c7f41a38081e4b20a56d0
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2021-05-06 00:40:28 +0300

    task/BS-04-22: Add first invalid context for specs

--- spec/controllers/addresses_controller_spec.rb
+++ spec/controllers/addresses_controller_spec.rb
@@ -6,21 +6,14 @@ RSpec.describe AddressesController, type: :controller do
   describe 'POST /addresses' do
     before { post :create, params: { address: params } }
 
-    context 'when save billing address with valid params' do
-      let(:params) { attributes_for(:address) }
-
-      it { expect(response).to redirect_to(settings_path) }
-      it { expect(response).to have_http_status(:found) }
-    end
-
     context 'when save billing address with invalid params' do
       let(:params) { attributes_for(:address, city: '') }
 
       it { expect(response).to have_http_status(:ok) }
     end
 
-    context 'when save shipping address with valid params' do
-      let(:params) { attributes_for(:address, address_type: 'shipping') }
+    context 'when save billing address with valid params' do
+      let(:params) { attributes_for(:address) }
 
       it { expect(response).to redirect_to(settings_path) }
       it { expect(response).to have_http_status(:found) }
@@ -30,6 +23,13 @@ RSpec.describe AddressesController, type: :controller do
       let(:params) { attributes_for(:address, address_type: 'shipping', city: '') }
 
       it { expect(response).to have_http_status(:ok) }
+    end
+
+    context 'when save shipping address with valid params' do
+      let(:params) { attributes_for(:address, address_type: 'shipping') }
+
+      it { expect(response).to redirect_to(settings_path) }
+      it { expect(response).to have_http_status(:found) }
     end
   end
 end
--- spec/forms/address_form_spec.rb
+++ spec/forms/address_form_spec.rb
@@ -12,16 +12,16 @@ RSpec.describe AddressForm, type: :model do
     it { is_expected.to validate_length_of(:zip_code).is_at_most(described_class::ZIP_MAX_SIZE) }
     it { is_expected.to validate_length_of(:phone).is_at_most(described_class::PHONE_MAX_SIZE) }
 
-    context 'when params are valid' do
-      let(:valid_address_params) { attributes_for(:address) }
-
-      it { expect(described_class.new(valid_address_params)).to be_valid }
-    end
-
     context 'when params validation fails' do
       let(:invalid_address_params) { attributes_for(:address, first_name: '!$&!') }
 
       it { expect(described_class.new(invalid_address_params)).to be_invalid }
+    end
+
+    context 'when params are valid' do
+      let(:valid_address_params) { attributes_for(:address) }
+
+      it { expect(described_class.new(valid_address_params)).to be_valid }
     end
   end
 end
