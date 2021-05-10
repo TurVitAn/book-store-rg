bb74d469cd4dd4954230125838e58649b765f5ce
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2021-05-10 17:15:14 +0300

    task/BS-06-09: Dry bath and member actions for admin Review

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -13,6 +13,21 @@ ActiveAdmin.register Review do
 
   config.filters = false
 
+  controller do
+    private
+
+    def update_review_status(status:, ids:)
+      batch_action_collection.find(ids).each(&status.to_sym)
+      redirect_back(fallback_location: admin_reviews_path)
+    end
+
+    def update_review_status_for_member_action(status)
+      review = Review.find(permitted_params[:id])
+      review.update(status: status)
+      redirect_to admin_review_path(review)
+    end
+  end
+
   index do
     selectable_column
     id_column
@@ -25,32 +40,26 @@ ActiveAdmin.register Review do
   end
 
   batch_action :approve, if: proc { @current_scope.scope_method != :approved } do |ids|
-    batch_action_collection.find(ids).each(&:approved!)
-    redirect_back(fallback_location: admin_reviews_path)
+    update_review_status(status: :approved!, ids: ids)
   end
 
   batch_action :reject, if: proc { @current_scope.scope_method != :rejected } do |ids|
-    batch_action_collection.find(ids).each(&:rejected!)
-    redirect_back(fallback_location: admin_reviews_path)
+    update_review_status(status: :rejected!, ids: ids)
   end
 
   action_item :approve, only: :show do
-    link_to 'Approve', approve_admin_review_path(review), method: :put unless review.approved?
+    link_to t('links.approve'), approve_admin_review_path(review), method: :put unless review.approved?
   end
 
   action_item :reject, only: :show do
-    link_to 'Reject', reject_admin_review_path(review), method: :put unless review.rejected?
+    link_to t('links.reject'), reject_admin_review_path(review), method: :put unless review.rejected?
   end
 
   member_action :approve, method: :put do
-    review = Review.find(permitted_params[:id])
-    review.update(status: :approved)
-    redirect_to admin_review_path(review)
+    update_review_status_for_member_action(:approved)
   end
 
   member_action :reject, method: :put do
-    review = Review.find(permitted_params[:id])
-    review.update(status: :rejected)
-    redirect_to admin_review_path(review)
+    update_review_status_for_member_action(:rejected)
   end
 end
