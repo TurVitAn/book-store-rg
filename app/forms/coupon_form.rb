02c71b3d538e949e502c5b39f4ef86600b1be3ab
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2021-05-19 19:53:44 +0300

    task/BS-08-12: Add CouponForm

---
+++ app/forms/coupon_form.rb
@@ -0,0 +1,28 @@
+class CouponForm
+  include ActiveModel::Model
+
+  attr_accessor :code
+
+  validates :code, presence: true
+  validate :coupon_is_not_exists
+  validate :coupon_is_used, if: :coupon
+  validate :coupon_is_invalid, if: :coupon
+
+  private
+
+  def coupon
+    @coupon ||= Coupon.find_by(code: code)
+  end
+
+  def coupon_is_not_exists
+    errors.add(:code, I18n.t('coupons.alert.not_exist')) unless coupon
+  end
+
+  def coupon_is_used
+    errors.add(:code, I18n.t('coupons.alert.used')) if coupon.cart
+  end
+
+  def coupon_is_invalid
+    errors.add(:code, I18n.t('coupons.alert.invalid')) unless coupon.is_valid
+  end
+end
--- app/services/carts/coupon_service.rb
+++ app/services/carts/coupon_service.rb
@@ -2,24 +2,24 @@ module Carts
   class CouponService
     attr_reader :errors
 
-    def initialize(code, cart)
-      @code = code
+    def initialize(params, cart)
+      @coupon_form = CouponForm.new(params)
       @cart = cart
-      @errors = []
     end
 
     def call
-      coupon = Coupon.find_by(code: @code)
-      check_coupon(coupon)
-      @cart.update(coupon: coupon) if @errors.empty?
+      if coupon_form.invalid?
+        @errors = coupon_form.errors
+      else
+        coupon = Coupon.find_by(code: coupon_form.attributes[:code])
+        coupon.update(cart: cart)
+      end
+
+      errors.blank?
     end
 
     private
 
-    def check_coupon(coupon)
-      @errors << I18n.t('coupons.alert.not_exist') if coupon.nil?
-      @errors << I18n.t('coupons.alert.used') if coupon.cart.present?
-      @errors << I18n.t('coupons.alert.invalid') unless coupon.is_valid
-    end
+    attr_reader :cart, :coupon_form
   end
 end
