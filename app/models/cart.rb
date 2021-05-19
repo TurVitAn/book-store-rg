911f1bbf6121f749272cd194bd35f893f5f05545
Vitalii Turzhanskyi <turzhansky81@gmail.com>
2021-05-19 19:56:44 +0300

    task/BS-08-13: Change dependency on cart destroy to nullify

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,5 @@
 class Cart < ApplicationRecord
   belongs_to :user, optional: true
-  has_one :coupon, dependent: :destroy
+  has_one :coupon, dependent: :nullify
   has_many :cart_items, dependent: :destroy
 end
