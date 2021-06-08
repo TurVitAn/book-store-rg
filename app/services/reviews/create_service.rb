d6292c249cb4faf8a3869f9599a7930a5889ac4a
Vitalii Turzhanskyi <vitalii.turzhanskyi@gmail.com>
2021-06-08 15:20:30 +0300

    task/BS-06-10: Refactor call method review create service

--- app/services/reviews/create_service.rb
+++ app/services/reviews/create_service.rb
@@ -8,13 +8,20 @@ module Reviews
     end
 
     def call
-      review_form.valid? ? Review.create(params) : @errors = review_form.errors
-
-      errors.blank?
+      review_form.valid? ? review_create(params) : collect_errors
     end
 
     private
 
     attr_reader :params, :review_form
+
+    def review_create(params)
+      Review.create(params)
+    end
+
+    def collect_errors
+      @errors = review_form.errors
+      errors.blank?
+    end
   end
 end
