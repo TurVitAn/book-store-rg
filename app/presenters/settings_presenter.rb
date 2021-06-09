f485f3755956b7de77df396812e73b4a7c4040bd
Vitalii Turzhanskyi <vitalii.turzhanskyi@gmail.com>
2021-06-10 00:49:39 +0300

    task/BS-10-11: Add AddressBasePresenter

--- app/presenters/settings_presenter.rb
+++ app/presenters/settings_presenter.rb
@@ -1,7 +1,7 @@
-class SettingsPresenter
+class SettingsPresenter < AddressBasePresenter
   def initialize(user:, params: {}, address_errors: {})
-    @user = user
-    @params = params
+    super(user: user, params: params)
+
     @address_errors = address_errors
   end
 
@@ -21,13 +21,9 @@ class SettingsPresenter
     @shipping_errors ||= check_address_type_errors(:shipping)
   end
 
-  def countries
-    @countries ||= ISO3166::Country.all.sort_by(&:name)
-  end
-
   private
 
-  attr_reader :user, :params, :address_errors
+  attr_reader :address_errors
 
   def check_address_type_errors(address_type)
     address_errors if params[:address_type] == address_type.to_s
