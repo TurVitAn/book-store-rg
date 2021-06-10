bb5376ad582e9ff680fc3d7a317b6ea12e8bb431
Vitalii Turzhanskyi <vitalii.turzhanskyi@gmail.com>
2021-06-10 18:10:21 +0300

    task/BS-05-09: Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,16 @@ CATEGORIES = {
   web_development: 'Web Development'
 }.freeze
 
-CATEGORIES.each_value { |category| Category.create(name: category) } if Category.count.zero?
+CATEGORIES.each_value { |category| Category.create(name: category) } unless Category.exists?
 
-FactoryBot.create_list(:author, 7) if Author.count.zero?
+FactoryBot.create_list(:author, 7) unless Author.exists?
 
-if Book.count.zero?
+unless Book.exists?
   50.times do
     FactoryBot.create(:book, category_id: rand(1..4), authors: [Author.all.sample, Author.all.sample])
   end
 end
 
-if AdminUser.count.zero?
+unless AdminUser.exists?
   AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 end
