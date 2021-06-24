bb191665d7ad230e5da1cf80edcab67f881d8489
Vitalii Turzhanskyi <vitalii.turzhanskyi@gmail.com>
2021-06-24 16:19:03 +0300

    task/BS-08-33: Fix styles order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
-  enum status: { pending: 0 }
-
   belongs_to :user, optional: true
   has_one :coupon, dependent: :nullify
   has_many :order_items, dependent: :destroy
+
+  enum status: { pending: 0 }
 end
