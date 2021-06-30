ea5d37aa881f965dfa8e7bb9d0921615278215a2
TurVitAn <vitalii.turzhanskyi@gmail.com>
2021-06-30 15:44:11 +0300

    task/BS-10-12: Fix rebase conflicts

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,14 +7,14 @@ class Order < ApplicationRecord
   has_one :credit_card, dependent: :destroy
   has_many :order_items, dependent: :destroy
 
-  enum status: { pending: 0, address: 1, delivery: 2, payment: 3, confirmation: 4, complete: 5 }
+  enum status: { pending: 0, address: 1, delivery: 2, payment: 3, confirm: 4, complete: 5 }
 
   aasm column: :status, enum: true do
     state :pending, initial: true
     state :address
     state :delivery
     state :payment
-    state :confirmation
+    state :confirm
     state :complete
 
     event :to_address do
@@ -29,12 +29,12 @@ class Order < ApplicationRecord
       transitions from: :delivery, to: :payment
     end
 
-    event :confirmation do
-      transitions from: :payment, to: :confirmation
+    event :confirm do
+      transitions from: :payment, to: :confirm
     end
 
     event :complete do
-      transitions from: :confirmation, to: :complete
+      transitions from: :confirm, to: :complete
     end
   end
 end
